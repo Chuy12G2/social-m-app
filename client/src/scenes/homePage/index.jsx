8bdbd8e85237d0cac102bdfaf8a01241922ee9a1
jesus <chuygs12@gmail.com>
2023-11-27 11:32:01 -0600

    navbar

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,10 @@
+import { Box } from "@mui/material"
+import Navbar from "scenes/navbar"
+
 const HomePage = () => {
-  return (<div>Homepage</div>)
+  return(
+    <Navbar/>
+  )
 }
 
 export default HomePage
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,21 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { 
-  mode: "light", 
+const initialState = {
+  mode: "light",
   user: null,
   token: null,
   posts: [],
 };
 
-const authSlice = createSlice({
+export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     setMode: (state) => {
-      state.mode =  state.mode === "light" ? "dark" : "light";
+      state.mode = state.mode === "light" ? "dark" : "light";
     },
     setLogin: (state, action) => {
-      state.user = action.payload;
+      state.user = action.payload.user;
       state.token = action.payload.token;
     },
     setLogout: (state) => {
@@ -23,25 +23,25 @@ const authSlice = createSlice({
       state.token = null;
     },
     setFriends: (state, action) => {
-      if(state.user){
-        state.user.friends = action.payload
-      }else{
-        console.error("user friends non-existent")
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("user friends non-existent :(");
       }
     },
     setPosts: (state, action) => {
-      state.posts = action.payload.posts
+      state.posts = action.payload.posts;
     },
     setPost: (state, action) => {
       const updatedPosts = state.posts.map((post) => {
-        if(post._id === action.payload.post_id) return action.payload.post
-        return post
-      })
-      state.posts = updatedPosts
-    }
-    }
-})
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
 
-export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions
-
-export default authSlice.reducer
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+export default authSlice.reducer;
